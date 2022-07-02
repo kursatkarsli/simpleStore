91574b6830e214a6d9756665e2458701679e2b3c
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2022-07-02 20:53:24 +0300

    cart has changed

--- src/assets/materialUi/CustomCard.jsx
+++ src/assets/materialUi/CustomCard.jsx
@@ -3,5 +3,6 @@ import { Card } from '@mui/material'
 export const CustomCard = styled(Card)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'space-between',
   padding: '50px',
 }))
